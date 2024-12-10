10b5cf8a6e0a966faff2c42c274b2bf0362e308c
Perdana Main <firman.fp123@gmail.com>
2024-12-10 23:08:25 +0700

    feat: add config at database

---
+++ src/config/database.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+
+class Database {
+  constructor() {
+    if (Database.instance) {
+      return Database.instance;
+    }
+
+    this.prisma = new PrismaClient({
+      log: ["query", "info", "warn", "error"],
+      errorFormat: "pretty",
+    });
+
+    // handle disconnect
+    process.on("beforeExit", async () => {
+      await this.$disconnect();
+    });
+
+    Database.instance = this;
+  }
+
+  getInstances() {
+    return this.prisma;
+  }
+
+  async connect() {
+    try {
+      await this.prisma.$connect();
+      console.log("Database connected");
+    } catch (error) {
+      console.error("Database connection error", error);
+      throw error;
+    }
+  }
+
+  async $disconnect() {
+    try {
+      await this.prisma.$disconnect();
+      console.log("Database disconnected");
+    } catch (error) {
+      console.error("Database disconnect error", error);
+      throw error;
+    }
+  }
+}
+
+export default new Database();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import app from "./app.js";
 import config from "./config/index.js";
+import db from "./config/database.js";
 
 const port = config.port;
 
-app.listen(port, () => {
-  console.log(`
-    ################################################
-    🛡️  Server listening on port: ${port} 🛡️
-    ################################################
-    `);
-});
+async function main() {
+  try {
+    await db.connect();
+    app.listen(port, () => {
+      console.log(`
+        ################################################
+        🛡️  Server listening on port: ${port} 🛡️
+        ################################################
+        `);
+    });
+  } catch (error) {
+    console.error("Error starting server", error);
+    process.exit(1);
+  }
+}
+
+main();
