6052d6c9a540cfd09e21b55039a4945d1776e2c4
perdanamain <firman.fp123@gmail.com>
2025-04-09 21:32:39 +0700

    feat: set up inventory

---
+++ src/app/admin/inventory/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="px-8 py-6">
+      {/* attendance */}
+      <div className="bg-white rounded-2xl w-full px-6 py-4 flex items-center text-gray-800 justify-between ">
+        <div className="flex items-center ">
+          <h1 className="text-lg font-semibold">Inventory</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "@/components/admin/Sidebar";
+import Navbar from "@/components/admin/Navbar";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="grid grid-cols-5 max-w-screen-2xl mx-auto border-x-2 border-gray-100">
+        <Sidebar />
+
+        {/* Main */}
+        <div className="col-span-4 bg-gray-100 ">
+          <Navbar />
+          {/* Main Section */}
+          {children}
+        </div>
+        {/* /Main */}
+      </div>
+    </>
+  );
+}
