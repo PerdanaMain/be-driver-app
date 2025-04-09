01738a912599c79e3d35d2078bca1abb4373184e
perdanamain <firman.fp123@gmail.com>
2025-04-09 21:39:52 +0700

    feat: set up hero ui

--- src/app/admin/inventory/page.tsx
+++ src/app/admin/inventory/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
 import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@heroui/react";
 
 const Page = () => {
   return (
@@ -8,6 +18,37 @@ const Page = () => {
         <div className="flex items-center ">
           <h1 className="text-lg font-semibold">Inventory</h1>
         </div>
+      </div>
+      <div className="bg-white rounded-2xl w-full px-6 py-4 flex items-center text-gray-800 justify-between mt-4">
+        <Table aria-label="Example static collection table">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Zoey Lang</TableCell>
+              <TableCell>Technical Lead</TableCell>
+              <TableCell>Paused</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>Jane Fisher</TableCell>
+              <TableCell>Senior Developer</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>William Howard</TableCell>
+              <TableCell>Community Manager</TableCell>
+              <TableCell>Vacation</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </main>
   );
